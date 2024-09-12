d913b27d127d3a25bca466e4b70bb2789e481e61
Hussein380 <huznigarane@gmail.com>
2024-09-12 07:58:40 +0300

    new logic

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,39 @@
-def rotate_2d_matrix(matrix):
-    n = len(matrix)
-    # Step 1: Transpose the matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
 
-    # Step 2: Reverse each row
-    for i in range(n):
-        matrix[i].reverse()
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a square 2D matrix (n x n) 90 degrees clockwise in place.
+
+    Args:
+        matrix (list of list of int): The n x n matrix to rotate.
+    """
+    n = len(matrix)  # Get the size of the matrix (n x n)
+
+    # Loop through each layer (from outer to inner)
+    for layer in range(n // 2):
+        # Define the first and last index for the current layer
+        first = layer
+        last = n - layer - 1
+
+        # Loop through each element in the current layer
+        for i in range(first, last):
+            offset = i - first  # Calculate how far we are from the edge
+
+            # Save the top element temporarily (will be overwritten)
+            top = matrix[first][i]
+
+            # Move the left element to the top
+            matrix[first][i] = matrix[last - offset][first]
+
+            # Move the bottom element to the left
+            matrix[last - offset][first] = matrix[last][last - offset]
+
+            # Move the right element to the bottom
+            matrix[last][last - offset] = matrix[i][last]
+
+            # Move the top element (from the temp variable) to the right
+            matrix[i][last] = top
