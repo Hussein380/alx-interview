3b0821486a89eff93485863b07071b4e059679f9
Hussein380 <huznigarane@gmail.com>
2024-09-21 08:17:09 +0300

    made sure the file is executable, implemented pep 8

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 
-"""Module containing the make_change function to solve the coin change problem."""
+"""Module containing the make_change function to solve the coin change
+problem."""
 
 
 def make_change(coins, total):
     """
-    Determine the minimum number of coins needed to achieve the given total amount.
-    
+    Determine the minimum number of coins needed to achieve the given
+    total amount.
+
     Args:
     coins (list): A list of available coin denominations.
     total (int): The target amount to achieve.
 
     Returns:
-    int: The fewest number of coins needed to meet the total. 
+    int: The fewest number of coins needed to meet the total.
          Returns 0 if the total is 0 or less.
-         Returns -1 if it's not possible to meet the total with the available coins.
+         Returns -1 if it's not possible to meet the total with the
+         available coins.
     """
-    
+
     # If no coins are provided, return -1 (impossible to make change)
     if not coins or coins is None:
         return -1
-    
+
     # If total is zero or less, no coins are needed
     if total <= 0:
         return 0
@@ -37,11 +40,10 @@ def make_change(coins, total):
         while coin <= total:
             total -= coin
             coin_count += 1
-        
+
         # If total reaches zero, return the number of coins used
         if total == 0:
             return coin_count
 
-    # If we exit the loop and total is not zero, return -1 (change can't be made)
+    # If we exit the loop and total is not zero, return -1
     return -1
-
